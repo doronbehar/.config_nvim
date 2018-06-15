180828cc50b669e4f129924867ced8dae89ef180
Doron Behar <doron.behar@gmail.com>
2018-06-15 12:52:28 +0300

    Update doc/ tags conditionaly in update-submodules.

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git submodule foreach 'git pull origin master && nvim --headless -c "helptags doc" -c "quit" || :'
+git submodule foreach 'git pull origin master && [[ -d doc ]] && nvim --headless -c "helptags doc" -c "quit" || :'
