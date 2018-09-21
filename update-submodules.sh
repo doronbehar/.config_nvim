9855e7a114e88b91fa27d5886ed5a4cbe44daea2
Doron Behar <doron.behar@gmail.com>
2018-09-21 19:39:38 +0300

    Make update-submodules script work with $EDITOR
    Not just nvim.

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git submodule foreach 'git branch | grep -q "^* master$" && git pull origin master && [[ -d doc ]] && nvim --headless -c "helptags doc" -c "quit" && git -C ../.. add $path || :'
+git submodule foreach 'git branch | grep -q "^* master$" && git pull origin master && [[ -d doc ]] && $EDITOR -es --cmd "helptags doc" --cmd "quit" && git -C ../.. add $path || :'
