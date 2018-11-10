1d46e997dfa1df2e52a8ac72f74a1a4985b4c6ab
Doron Behar <doron.behar@gmail.com>
2018-11-10 18:52:44 +0200

    Check for gitlab and github ssh keys in agent

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
+for k in ~/.ssh/git{hub,lab}; do
+	grep --file <(ssh-add -L | cut -d' ' -f2) ~/.ssh/*.pub -l | grep -q "$k" || ssh-add "$k"
+done
 git submodule foreach 'git branch | grep -q "^* master$" && git pull origin master && [[ -d doc ]] && $EDITOR -es --cmd "helptags doc" --cmd "quit" && git -C ../.. add $path || :'
