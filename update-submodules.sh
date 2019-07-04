4b361770d3b03a38ee41f7db82fc8597da4697ca
Doron Behar <doron.behar@gmail.com>
2019-07-04 12:24:30 +0300

    Simplify update-submodules settings

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-git submodule foreach 'git branch | grep -q "^* master$" && git pull origin master && [[ -d doc ]] && $EDITOR -es --cmd "helptags doc" --cmd "quit" && git -C ../.. add $path || :'
+git submodule foreach 'git pull && [[ -d doc ]] && $EDITOR -es --cmd "helptags doc" --cmd "quit" && git -C ../.. add $path || :'
