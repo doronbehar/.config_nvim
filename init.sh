926a84429e78a4e6ffbaf1d1f759850edd8350a2
Doron Behar <doron.behar@gmail.com>
2019-07-22 11:31:01 +0300

    Use tpope's dispatch and the Neovim adapter
    Replace my fork of @tpope's fugitive which has a minimalistic version of
    dispatch in it and use the displatch neovim adapter which provides use
    of `:term` instead.

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script modifies fugitive's git push URL for bootstrapping purposes
-
-git -C bundle/fugitive remote add origin https://github.com/tpope/vim-fugitive
-git -C bundle/fugitive config branch.master.remote origin
-git -C bundle/fugitive config branch.master.pushremote doronbehar
