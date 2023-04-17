f7200f012aeebed6cd46053de37de1b062e7c487
Doron Behar <doron.behar@gmail.com>
2023-04-18 01:09:01 +0300

    Fix path in ./update-submodules.sh

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git submodule foreach 'git pull && [[ -d doc ]] && $EDITOR -es --cmd "helptags doc" --cmd "quit" && git -C ../.. add $path || :'
+git submodule foreach 'git pull && [[ -d doc ]] && $EDITOR -es --cmd "helptags doc" --cmd "quit" && git -C ../../../.. add $path || :'
