805be4ceb7fb91e697514f3901bdc434d5a20721
Doron Behar <doron.behar@gmail.com>
2025-06-25 16:32:14 +0300

    Slightly increase bufIsBig limit

--- host-specific/ZENIX.lua
+++ host-specific/ZENIX.lua
@@ -103,7 +103,7 @@ vim.keymap.set('v', '<leader>ds', dap_python.debug_selection, {desc = "DAP_PYTHO
 
 -- {{{ Helper for treesitter, and cmp
 bufIsBig = function(bufnr)
-	local max_filesize = 100 * 1024 -- 100 KB
+	local max_filesize = 200 * 1024 -- 100 KB
 	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
 	if ok and stats and stats.size > max_filesize then
 		return true
